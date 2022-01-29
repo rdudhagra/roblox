f622f19e03c24df36426565b3e2e555f4d2a69f3
Jeff Tan <jefftan969@gmail.com>
2022-01-29 18:40:03 -0500

    support fps lower than 30

--- Vision/camera_feed.py
+++ Vision/camera_feed.py
@@ -1,11 +1,12 @@
 import cv2
 import argparse
 
+
 parser = argparse.ArgumentParser()
 parser.add_argument("--cam_port", "-p", type=int, default=0, help="OpenCV camera port")
 parser.add_argument("--cap_width", type=int, default=10000, help="Camera capture width")
 parser.add_argument("--cap_height", type=int, default=10000, help="Camera capture height")
-parser.add_argument("--cap_fps", type=int, default=30, help="Camera capture FPS")
+parser.add_argument("--cap_fps", type=int, default=15, help="Camera capture FPS")
 args = parser.parse_args()
 
 cam = cv2.VideoCapture(args.cam_port)
@@ -17,7 +18,10 @@ cam.set(cv2.CAP_PROP_FRAME_HEIGHT, args.cap_height)
 cam.set(cv2.CAP_PROP_FPS, args.cap_fps)
 
 while True:
-    (ret, frame) = cam.read()
+    frames_per_loop = 30 // args.cap_fps
+    for i in range(frames_per_loop):
+        cam.grab()
+    (ret, frame) = cam.retrieve()
     cv2.imshow("frame", frame)
     print("Frame captured: ", frame.shape)
     if cv2.waitKey(1) & 0xFF == ord('q'):
