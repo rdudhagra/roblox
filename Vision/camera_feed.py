f57d1b6eff8566b5fd846b69fce7d0db48e3903b
Jeff Tan <jefftan969@gmail.com>
2022-01-29 15:43:30 -0500

    set camera properties

--- Vision/camera_feed.py
+++ Vision/camera_feed.py
@@ -1,10 +1,25 @@
 import cv2
+import argparse
 
-cam = cv2.VideoCapture(0)
+parser = argparse.ArgumentParser()
+parser.add_argument("--cam_port", "-p", type=int, default=0, help="OpenCV camera port")
+parser.add_argument("--cap_width", type=int, default=10000, help="Camera capture width")
+parser.add_argument("--cap_height", type=int, default=10000, help="Camera capture height")
+parser.add_argument("--cap_fps", type=int, default=30, help="Camera capture FPS")
+args = parser.parse_args()
+
+cam = cv2.VideoCapture(args.cam_port)
+
+# Set camera properties
+cam.set(cv2.CAP_PROP_FOURCC, cv2.VideoWriter_fourcc('M', 'J', 'P', 'G'));
+cam.set(cv2.CAP_PROP_FRAME_WIDTH, args.cap_width)
+cam.set(cv2.CAP_PROP_FRAME_HEIGHT, args.cap_height)
+cam.set(cv2.CAP_PROP_FPS, args.cap_fps)
 
 while True:
     (ret, frame) = cam.read()
     cv2.imshow("frame", frame)
+    print("Frame captured: ", frame.shape)
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
 
