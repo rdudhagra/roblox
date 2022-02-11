675b4b43c45cc04c4c7681937c5dfad36fa3aa6e
Ravi Dudhagra <rdudhagra@gmail.com>
2022-02-10 22:59:14 -0500

    better cube detection

--- Vision/detect_cubes.py
+++ Vision/detect_cubes.py
@@ -2,46 +2,21 @@ import cv2
 import numpy as np
 import pickle
 
-# Setup SimpleBlobDetector parameters.
-blobDetectorParams = cv2.SimpleBlobDetector_Params()
-
-# Change thresholds
-blobDetectorParams.minThreshold = 127
-blobDetectorParams.maxThreshold = 129
-blobDetectorParams.thresholdStep = 1
-
-# Filter by Area.
-blobDetectorParams.filterByArea = True
-blobDetectorParams.minArea = 5000
-blobDetectorParams.maxArea = 500000
-
-# Filter by Circularity
-blobDetectorParams.filterByCircularity = False
-# blobDetectorParams.minCircularity = 0.2
-# blobDetectorParams.maxCircularity = 1.0
-
-# Filter by Convexity
-blobDetectorParams.filterByConvexity = True
-blobDetectorParams.minConvexity = 0.87
-blobDetectorParams.maxConvexity = 1.0
-
-# Filter by Inertia
-blobDetectorParams.filterByInertia = True
-blobDetectorParams.minInertiaRatio = 0.01
-blobDetectorParams.maxInertiaRatio = 1
-
-# Filter by Color
-blobDetectorParams.filterByColor = True
-blobDetectorParams.blobColor = 255
-
-# Create a detector with the parameters
-detector = cv2.SimpleBlobDetector_create(blobDetectorParams)
-
+from video_capture_threading import VideoCaptureThreading
 
 # Read cube calibration data
 with open("cube_calibration_data.pkl", "rb") as f:
     [cube_color_mins, cube_color_maxs] = pickle.load(f)
 
+def preprocess_frame(frame):
+    # Convert to HSV
+    hsv_img = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
+
+    # Sharpen
+    frame_sharpen = cv2.GaussianBlur(frame, (0, 0), 1)
+    frame_sharpen = cv2.addWeighted(frame, 1.5, frame_sharpen, -0.5, 0)
+
+    return hsv_img
 
 def threshold_for_color(hsv_img, color):
     (low_H, low_S, low_V) = cube_color_mins[color]
@@ -62,20 +37,14 @@ def threshold_for_color(hsv_img, color):
         frame_threshold, cv2.MORPH_OPEN, cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (5, 5)))
     return frame_threshold
 
-
-def detect_blobs(threshold_img):
-    # Detect blobs.
-    keypoints = detector.detect(threshold_img)
-    return keypoints
-
+def detect_squares(threshold_img) -> "list(cv2.RotatedRect)":
+    contours, _ = cv2.findContours(threshold_img, cv2.RETR_LIST, cv2.CHAIN_APPROX_SIMPLE)
+    squares = [cv2.minAreaRect(c) for c in contours if cv2.contourArea(c) > 5000]
+    return squares
 
 if __name__ == "__main__":
     # Read frames from webcam
-    cap = cv2.VideoCapture(0)
-    cap.set(cv2.CAP_PROP_FOURCC, cv2.VideoWriter_fourcc(*'MJPG'))
-    cap.set(cv2.CAP_PROP_FRAME_WIDTH, 3840)
-    cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 2160)
-    cap.set(cv2.CAP_PROP_FPS, 30)
+    cap = VideoCaptureThreading(src=4, width=3840, height=2160).start()
 
     # Create a window
     cv2.namedWindow("frame", cv2.WINDOW_NORMAL)
@@ -84,27 +53,28 @@ if __name__ == "__main__":
     while True:
         # Read frame
         ret, frame = cap.read()
-
-        final_img = frame.copy()
-
-        # Convert to HSV
-        hsv_img = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
+        hsv_img = preprocess_frame(frame)
 
         # Show frame
         for color in ["red", "green", "blue", "yellow", "purple", "orange"]:
             thresh_img = threshold_for_color(hsv_img, color)
-            keypoints = detect_blobs(thresh_img)
+            # keypoints = detect_blobs(thresh_img)
 
-            nice_debug_img = cv2.max(frame, np.repeat(
-                thresh_img[:, :, np.newaxis], 3, axis=2))
-            for keypoint in keypoints:
-                cv2.circle(final_img, (int(keypoint.pt[0]), int(keypoint.pt[1])),
-                        int(keypoint.size/2), (0, 0, 0), 5)
-        cv2.imshow("frame", final_img)
+            # nice_debug_img = cv2.max(frame, np.repeat(
+            #     thresh_img[:, :, np.newaxis], 3, axis=2))
+            # for keypoint in keypoints:
+            #     cv2.circle(final_img, (int(keypoint.pt[0]), int(keypoint.pt[1])),
+            #             int(keypoint.size/2), (0, 0, 0), 5)
+
+            # Find contours
+            squares = detect_squares(thresh_img)
+            for sq in squares:
+                cv2.drawContours(frame, [np.int0(cv2.boxPoints(sq))], 0, (0, 0, 0), 3)
+        cv2.imshow("frame", frame)
 
         # Check if 'q' is pressed
         if cv2.waitKey(1) & 0xFF == ord('q'):
             break
 
-    # Release the camera
-    cap.release()
+    # Stop the camera
+    cap.stop()
